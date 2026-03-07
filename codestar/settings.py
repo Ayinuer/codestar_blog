4cf14467e3ce3e4486dc55abd92c8dd70d159e60
Ayinuer <aynurehet508@gmail.com>
2026-03-07 14:38:56 +0000

    Set SECRET_KEY from environment

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p&4_9=r2g86!t3b=f!a#(d63-=&qu21hc@m-rucyvbp$2xhai$'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
--- eny.py
+++ eny.py
@@ -1,4 +1,11 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "<postgresql://neondb_owner:npg_x6okzOByCwR9@ep-super-smoke-ago2nwc4.c-2.eu-central-1.aws.neon.tech/denim_ninth_gem_898777>")
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_x6okz0ByCwR9@ep-super-smoke-ago2nwc4.c-2.eu-central-1.aws.neon.tech/denim_ninth_gem_898777"
+)
+
+os.environ.setdefault(
+    "SECRET_KEY",
+    "9g#2Lx!7pA@4sF8dQ$k2Zp1&cR6vT3mYh5Nw8uB"
+)
