1d4e637fe6ac2f587eaf5c1817b70df39dec293e
Ayinuer <aynurehet508@gmail.com>
2026-03-07 13:43:33 +0000

    Prepare for Heroku deployment

---
+++ eny.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "<postgresql://neondb_owner:npg_x6okzOByCwR9@ep-super-smoke-ago2nwc4.c-2.eu-central-1.aws.neon.tech/denim_ninth_gem_898777>")
