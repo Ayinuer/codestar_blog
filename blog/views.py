304ad783d749f64a6ef0f9b580cd8d2d7039774f
Ayinuer <aynurehet508@gmail.com>
2026-03-10 21:16:39 +0000

    fix views import

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.views import generic
 from .models import Post
 
-class PostList(ListView):
+
+class PostList(generic.ListView):
     model = Post
     template_name = "blog/index.html"
     paginate_by = 6
